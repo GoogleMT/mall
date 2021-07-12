34552cd8aeb6df3dd54c02dda86058f1cffa1cc3
GoogleMT <2365897256@qq.com>
2021-07-12 23:57:09 +0800

    项目初始化

---
+++ mall-coupon/src/test/java/top/gumt/mall/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package top.gumt.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ mall-order/src/test/java/top/gumt/mall/order/MallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package top.gumt.mall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
