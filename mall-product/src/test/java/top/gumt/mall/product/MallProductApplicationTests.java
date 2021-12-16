df941506bbcf68012bb45fce439e648339819a48
GoogleMT <2365897256@qq.com>
2021-12-16 23:44:59 +0800

    delete AK

--- mall-product/src/test/java/top/gumt/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/top/gumt/mall/product/MallProductApplicationTests.java
@@ -60,8 +60,8 @@ class MallProductApplicationTests {
         // Endpoint以张家界为例，其它Region请按实际情况填写。
         String endpoint = "https://oss-cn-zhangjiakou.aliyuncs.com";
         // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录RAM控制台创建RAM账号。
-        String accessKeyId = "LTAI5tMojJ2baCmgV7QCuKMd";
-        String accessKeySecret = "i0dBM55lKd6uOSy4szZDfeAK4x8Ctn";
+        String accessKeyId = "accessKeyId";
+        String accessKeySecret = "accessKeySecret";
         // 创建OSSClient实例。
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
 // 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
